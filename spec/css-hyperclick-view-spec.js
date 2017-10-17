c7ebaf83a58ef2527fe6bfbf62eb20949c94d2fb
Alasdair McLeay <alasdair.mcleay@me.com>
2017-10-17 14:05:11 +0100

    Atom starter project

---
+++ spec/css-hyperclick-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CssHyperclickView from '../lib/css-hyperclick-view';
+
+describe('CssHyperclickView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
